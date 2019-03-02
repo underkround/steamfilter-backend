2d83681d798407087bfc9077d485c4a52adfd46c
Lauri Härsilä <murgo@iki.fi>
2019-03-02 05:13:07 +0200

    Improved error handling strategy in gamelist

--- src/gamelist/gamelist.go
+++ src/gamelist/gamelist.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http"
 
 	"github.com/aws/aws-lambda-go/events"
@@ -22,51 +21,36 @@ func fetchGameList(url string) (string, error) {
 	res, err := http.Get(url)
 
 	if err != nil {
-		log.Fatal(err)
 		return "", err
 	}
 
 	defer res.Body.Close()
 
 	if res.StatusCode != 200 {
-		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
 		return "", fmt.Errorf("Steam API response code: %s", res.StatusCode)
 	}
 
 	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return "", err
+	}
+
 	return string(body), err
 }
 
 func createGameListUrl(user string) (string, error) {
+	fmt.Printf("User: %s\n", user)
 	if user == "" {
-		log.Fatalf("Invalid user: %s", user)
-		return "", fmt.Errorf("Invalid user")
+		return "", fmt.Errorf("No user given")
 	}
 
 	// TODO: Different url types
-	url := fmt.Sprintf("https://steamcommunity.com/id/%s?xml=1", user)
+	url := fmt.Sprintf("https://steamcommunity.com/id/%s/games/?tab=all&xml=1", user)
 	return url, nil
 }
 
-func GetGameList(ctx context.Context, request Request) (Response, error) {
-	user := request.QueryStringParameters["user"]
-	url, err := createGameListUrl(user)
-	status := 200
-	var body string
-
-	if err != nil {
-		status = 403
-		body = err.Error()
-	} else {
-		body, err = fetchGameList(url)
-
-		if err != nil {
-			status = 403
-			body = err.Error()
-		}
-	}
-
-	resp := Response{
+func createResponse(status int, body string) Response {
+	return Response{
 		StatusCode:      status,
 		IsBase64Encoded: false,
 		Body:            body,
@@ -75,8 +59,22 @@ func GetGameList(ctx context.Context, request Request) (Response, error) {
 		//			"X-MyCompany-Func-Reply": "hello-handler",
 		//		},
 	}
+}
 
-	return resp, nil
+func GetGameList(ctx context.Context, request Request) (Response, error) {
+	user := request.QueryStringParameters["user"]
+	url, err := createGameListUrl(user)
+
+	if err != nil {
+		return createResponse(418, err.Error()), nil
+	}
+
+	body, err := fetchGameList(url)
+	if err != nil {
+		return createResponse(418, err.Error()), nil
+	}
+
+	return createResponse(200, body), nil
 }
 
 func main() {
